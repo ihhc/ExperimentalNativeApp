d6cd01311ae022468e053931a651a685f0b5acd2
Igor <ihhc@yandex.ru>
2022-11-30 15:19:26 +0300

    libpqxx connection added

--- PGSqlSmp/PGSqlSmp.cpp
+++ PGSqlSmp/PGSqlSmp.cpp
@@ -1,20 +1,26 @@
-// PGSqlSmp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <pqxx\pqxx>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string conn = "host=localhost port=5432 dbname=TestDB user=postgres password=postgres";
+	try
+	{
+		pqxx::connection connectionObject(conn.c_str());
+		pqxx::work pqworking(connectionObject);
+		pqxx::result res = pqworking.exec("SELECT * FROM TestTable");
+		for (int i = 0; res.size(); i++)
+			cout << res[i][0] << "	" << res[i][1];
+
+	}
+	catch (exception& e)
+	{
+		cout << e.what();
+	}
+	return 0;;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
